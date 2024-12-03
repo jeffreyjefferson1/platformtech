95c732f81e48be89474dfa35f3b16bdb60b19ce7
Todd Machacon <18103195@usc.edu.ph>
2024-12-03 22:35:15 +0800

    drop down

---
+++ PlatformTechnologyOkayKana/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlatformTechnologyOkayKana.Models;
+
+public partial class Product
+{
+    public int ProductId { get; set; }
+
+    public string ProductName { get; set; } = null!;
+
+    public string Category { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int QuantityInStock { get; set; }
+}
